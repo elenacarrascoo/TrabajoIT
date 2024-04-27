d0e86de13596b2997695b7fa81251cbb86908371
cbgclara <cbecgil@alu.upo.es>
2024-04-27 13:25:29 +0200

    Actualizacion dao Cita

--- TrabajoIT/src/java/persistencia/citaDAO.java
+++ TrabajoIT/src/java/persistencia/citaDAO.java
@@ -8,6 +8,81 @@ package persistencia;
  *
  * @author clarabecerragil
  */
+import java.util.List;
+import modelo.Cita;
+import modelo.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class citaDAO {
     
+    public void altaCita(Cita c){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.save(c);
+        tx.commit();
+    }
+    
+    public void bajaCita(Cita c){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.delete(c);
+        tx.commit();
+    }
+    
+    public void actualizarCita(Cita c){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.update(c);
+        tx.commit();
+    }
+    
+    public Cita obtenerCita(int idCita){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cita where id = " + idCita);
+        Cita c = (Cita) q.uniqueResult();
+        tx.commit();
+        return c;
+    }
+    
+    public Cita obtenerCita(int idCita){
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cita where id = " + idCita);
+        Cita c = (Cita) q.uniqueResult();
+        tx.commit();
+        return c;
+    }
+    
+    public List<Cita> obtenerCitasVeterinario(int idVeterinario){
+        List<Cita> citas;
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cita where idVeterinario = " + idVeterinario);
+        citas = (List<Cita>) q.list();
+        tx.commit();
+        return citas;
+    }
+    
+    public List<Cita> obtenerCitasPaciente(int numHistorial){
+        List<Cita> citas;
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cita where numHistorial = " + numHistorial);
+        citas = (List<Cita>) q.list();
+        tx.commit();
+        return citas;
+    }
+    
+    public List<Cita> obtenerCitasPendientes(int numHistorial, Date fecha, Time hora){
+        List<Cita> citas;
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cita where numHistorial = " + numHistorial +"AND fecha = " +fecha +"AND hora = " +hora);
+        citas = (List<Cita>) q.list();
+        tx.commit();
+        return citas;
+    } 
 }
